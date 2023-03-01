bdd3825e5e281106535ee5f36de00d46300aefae
Ben Sinclair <bensiclair@microsoft.com>
2023-03-01 14:55:45 +0000

    build script changes

--- src/build.rs
+++ src/build.rs
@@ -8,7 +8,6 @@ use cli::cli;
 fn main() {
     let cmd = cli();
 
-    // Invoked as: `$ my-app --markdown-help`
     let md = help_markdown_command(&cmd);
 
     File::create(Path::new("CLI_HELP.md"))
